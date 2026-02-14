a18c2a4a1753ddea3670010d0dde47ed41e1de33
Iqbal Fauzi <teamstac350@gmail.com>
2026-02-14 20:41:10 +0800

    feat: add s3 logs

--- infra/blod/main.tf
+++ infra/blod/main.tf
@@ -76,13 +76,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   comment             = "Some comment"
   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "mylogs.s3.amazonaws.com"
-    prefix          = "myprefix"
-  }
-
-  default_cache_behavior {
+default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.origin_id
